f98c803abca8377ce9eeb1922f0b014e32984622
Tom Rowe <theflyingcoder@hotmail.com>
2023-02-11 06:55:52 +1000

    Updating package to include better typing and extensibility. (#1)
    * Updating package to include better typing and extensibility.
    
    * no message
    
    * Update to not make this a breaking change, string scopes are still allowed, but Typed scopes would be available
    
    * Little clean up

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,54 @@
-import {
-  OAuth2Profile,
-  OAuth2Strategy,
-  OAuth2StrategyVerifyParams,
-} from 'remix-auth-oauth2';
+import { OAuth2Strategy, OAuth2StrategyVerifyParams } from 'remix-auth-oauth2';
 import type { StrategyVerifyCallback } from 'remix-auth';
 
-import type { FacebookPicture } from './types';
+import type {
+  AdditionalFacebookProfileField,
+  FacebookProfile,
+  FacebookScope,
+  FacebookExtraParams,
+  FacebookStrategyOptions,
+} from './types';
+export * from './types';
 
-/**
- * This interface declares what configuration the strategy needs from the
- * developer to correctly work.
- */
-export interface FacebookStrategyOptions {
-  clientID: string;
-  clientSecret: string;
-  callbackURL: string;
-  scope?: string;
-}
+export const baseProfileFields = [
+  'id',
+  'email',
+  'name',
+  'first_name',
+  'middle_name',
+  'last_name',
+  'picture',
+] as const;
 
-export type FacebookProfile = {
-  id: string;
-  displayName: string;
-  name: {
-    familyName: string;
-    givenName: string;
-  };
-  emails: [{ value: string }];
-  photos: [{ value: string }];
-  _json: {
-    id: string;
-    name: string;
-    first_name: string;
-    last_name: string;
-    picture: FacebookPicture;
-    email: string;
-  };
-} & OAuth2Profile;
-
-/**
- * This interface declares what the developer will receive from the strategy
- * to verify the user identity in their system.
- */
-export type FacebookExtraParams = {
-  // FIXME!
-} & Record<string, string | number>;
+export const FacebookStrategyName = 'facebook';
+export const FacebookStrategyDefaultScopes: FacebookScope[] = [
+  'public_profile',
+  'email',
+];
+export const FacebookStrategyScopeSeperator = ',';
+export type FacebookProfileFields = [
+  ...typeof baseProfileFields,
+  ...AdditionalFacebookProfileField[]
+];
 
 export class FacebookStrategy<User> extends OAuth2Strategy<
   User,
   FacebookProfile,
   FacebookExtraParams
 > {
-  public name = 'facebook';
-  private readonly scope: string;
+  public name = FacebookStrategyName;
+  private readonly scope: FacebookScope[];
   private readonly userInfoURL = 'https://graph.facebook.com/me';
 
-  private readonly profileFields = [
-    'id',
-    'email',
-    'name',
-    'first_name',
-    'last_name',
-    'picture',
-  ];
+  private readonly profileFields: FacebookProfileFields;
 
   constructor(
     {
       clientID,
       clientSecret,
       callbackURL,
-      scope = 'public_profile,email',
+      scope,
+      extraProfileFields,
     }: FacebookStrategyOptions,
     verify: StrategyVerifyCallback<
       User,
@@ -85,11 +65,29 @@ export class FacebookStrategy<User> extends OAuth2Strategy<
       },
       verify
     );
-    this.scope = scope;
+    this.scope = this.getScope(scope);
+    //Ensure unique entries in case they include the base fields
+    this.profileFields = [
+      ...new Set([...baseProfileFields, ...(extraProfileFields || [])]),
+    ] as FacebookProfileFields;
+  }
+
+  //Allow users the option to pass a scope string, or typed array
+  private getScope(scope: FacebookStrategyOptions['scope']) {
+    if (!scope) {
+      return FacebookStrategyDefaultScopes;
+    } else if (typeof scope === 'string') {
+      return scope.split(FacebookStrategyScopeSeperator) as FacebookScope[];
+    }
+
+    return scope;
   }
 
   protected authorizationParams(): URLSearchParams {
-    const params = new URLSearchParams({ scope: this.scope });
+    const params = new URLSearchParams({
+      scope: this.scope.join(FacebookStrategyScopeSeperator),
+    });
+
     return params;
   }
 
